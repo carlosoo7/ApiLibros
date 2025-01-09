6153656397f96b5b12ab046266235c4d696aca49
Carlos Fernando Calderon Mora <151805986+carlosoo7@users.noreply.github.com>
2025-01-09 07:34:17 -0500

    Creando proyecto

---
+++ src/main/java/com/CFCM/ConsultaLiteral/ConsultaLiteralApplication.java
@@ -0,0 +1,13 @@
+package com.CFCM.ConsultaLiteral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaLiteralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaLiteralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CFCM/ConsultaLiteral/ConsultaLiteralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CFCM.ConsultaLiteral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaLiteralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
